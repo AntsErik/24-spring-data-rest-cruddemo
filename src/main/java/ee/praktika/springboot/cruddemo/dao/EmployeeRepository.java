936e9bb26cedd69a6075d9c8b935d83e1dd6fcd5
AntsErik <ants.erik.noormagi@aweroc.com>
2019-10-28 15:42:59 +0200

    Changing the resource path for REST.

--- src/main/java/ee/praktika/springboot/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/ee/praktika/springboot/cruddemo/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package ee.praktika.springboot.cruddemo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import ee.praktika.springboot.cruddemo.entity.Employee;
 
+@RepositoryRestResource( path = "basketball-players" )
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     //no need to write any code, CRUD methods will be received for free.
